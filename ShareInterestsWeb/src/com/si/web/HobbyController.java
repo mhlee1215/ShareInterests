f88914505a20c1eaffe45b09641c4471fcccb9b8
mhlee <mhlee1215@gmail.com>
2016-08-31 13:20:42 -0700

    Drag&Drop file upload added.

--- ShareInterestsWeb/src/com/si/web/HobbyController.java
+++ ShareInterestsWeb/src/com/si/web/HobbyController.java
@@ -1,5 +1,6 @@
 package com.si.web;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +9,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -68,5 +73,30 @@ public class HobbyController {
 //		
 		return model;
     }
+	
+	@RequestMapping(value = "/fileUpload.do", method = RequestMethod.GET)
+    public String dragAndDrop(Model model) {
+         
+        return "fileUpload";
+         
+    }
+     
+    @RequestMapping(value = "/fileUpload/post.do") //ajax에서 호출하는 부분
+    @ResponseBody
+    public String upload(MultipartHttpServletRequest multipartRequest) { //Multipart로 받는다.
+         
+        Iterator<String> itr =  multipartRequest.getFileNames();
+         
+        while (itr.hasNext()) { //받은 파일들을 모두 돌린다.
+             
+            MultipartFile mpf = multipartRequest.getFile(itr.next());
+            String originFileName = mpf.getOriginalFilename();
+            System.out.println("FILE_INFO: "+originFileName); //받은 파일 리스트 출력
+             
+       }
+         
+        return "success";
+    }
+     
 	
 }
